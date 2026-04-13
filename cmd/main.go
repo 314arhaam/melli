bf6fe7dbe73a704c58955d1006f3f4df5bdaed41
Parham <prmbas@gmail.com>
2026-04-13 02:03:24 +0000

    goroutine works fine

--- cmd/main.go
+++ cmd/main.go
@@ -5,29 +5,56 @@ import (
 	"local/melli/internal/iotools"
 	"net/http"
 	"time"
+	"sync"
+	"encoding/json"
+	"os"
 )
 
-func PingWebsite(w iotools.Website) (int, int64, error) {
-	dt := time.Now().UnixMilli()
-	resp, err := http.Get(w.URL)
-	dt = time.Now().UnixMilli() - dt
-	// req.Header.Add("Content-Type", "application/json")
-	// client := &http.Client{}
-	// resp, err := client.Do(req)
-	if err != nil {
-		return -1, dt, fmt.Errorf("Error sending request: %v", err)
+func PingWebsite(website <- chan string, data chan <- iotools.Website, w *sync.WaitGroup) {
+	defer w.Done()
+	t0 := time.Now().UnixMilli()
+	for wb := range website {
+		resp, err := http.Get(wb)
+		t1 := time.Now().UnixMilli()
+		ping := int(t1-t0)
+		status := true
+		if err != nil || resp.StatusCode != 200 {
+			status = false
+		}
+		data <- iotools.Website{URL: wb, Ping: ping, StatusOK: status,}
 	}
-	// Ensure the response body is closed.
-	defer resp.Body.Close()
-	return resp.StatusCode, dt, nil
 }
 
 
 func main() {
 	data, _ := iotools.JsonFileToStr("data/d.json")
-	fmt.Println(data.Website[0])
+	fmt.Println(data.Website)
+	w := make(chan string, len(data.Website))
+	d := make(chan iotools.Website, len(data.Website))
+	var wg sync.WaitGroup
+	wg.Add(1)
 	dt := time.Now().UnixMilli()
-	s, t, _ := PingWebsite(data.Website[0])
+	go PingWebsite(w, d, &wg)
+	for i := 0; i < len(data.Website); i++ {
+		w <- data.Website[i].URL
+	}
+	close(w)
+	wg.Wait()
+	close(d)
+	out := iotools.WebsiteList{}
+	for i := range d {
+		fmt.Println("Done", i)
+		out.Website = append(out.Website, i)
+	}
 	dt = time.Now().UnixMilli() - dt
-	fmt.Println(s, t, dt)
+	out.Elapsed = int(dt)
+	fmt.Println(out)
+	file, err := os.Create("data/results.json")
+	if err != nil {
+		fmt.Errorf("Error in main, file creation error", err)
+	}
+	defer file.Close()
+	if err := json.NewEncoder(file).Encode(&out); err != nil {
+		fmt.Println(err)
+	}
 }
